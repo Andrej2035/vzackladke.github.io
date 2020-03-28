3cb06c24e3ec72e09b2fae67f5f2302c1f22812b
Andrej2035 <53010831+Andrej2035@users.noreply.github.com>
2020-03-28 14:06:53 +0200

    1111
    1111

---
+++ precache-manifest.bfd9ebccf5b787e8e7499106bfa933dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "476c126bf269ebd0730a042f8d811e83",
+    "url": "/Andrej2035/magazin.git/index.html"
+  },
+  {
+    "revision": "cfa5e6bf155b061c2f6f",
+    "url": "/Andrej2035/magazin.git/static/css/main.32a10329.chunk.css"
+  },
+  {
+    "revision": "5f44dfd52fbf1b3772b9",
+    "url": "/Andrej2035/magazin.git/static/js/2.648bfe4d.chunk.js"
+  },
+  {
+    "revision": "cfa5e6bf155b061c2f6f",
+    "url": "/Andrej2035/magazin.git/static/js/main.bf40a60b.chunk.js"
+  },
+  {
+    "revision": "55bbf9f2d58a4198f8aa",
+    "url": "/Andrej2035/magazin.git/static/js/runtime-main.2e84a80c.js"
+  }
+]);
